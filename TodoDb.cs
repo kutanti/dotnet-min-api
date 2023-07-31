3565fd27b85c176c8244503d73fe79be74fadcab
Kunal Tanti <ktanti@linkedin.biz>
2023-07-31 17:33:52 +0530

    Initial Commit.

---
+++ TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalAPI
+{
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
